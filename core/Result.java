43b3fc4026dc46f9be1eef84c9f34b99aa6d2227
POLARIS <yorushika.lyrah@gmail.com>
2026-02-24 21:12:37 +0700

    Update Result.java

--- core/Result.java
+++ core/Result.java
@@ -1,29 +1,27 @@
 // 680510716
+
 package core;
 
 public class Result {
-    // Attributes
-    private String playerName;
-    private int total;
-    private int score;
-    private String difficultyLevel;
+	// Attributes
+	private int total;
+	private int score;
 
-    // Construcotr
-    public Result(String name, int total, int score, String difficultyLevel) {
-        this.playerName = name;
-        this.total = total;
-        this.score = score;
-        this.difficultyLevel = difficultyLevel;
-    }
+	// Construcotr
+	public Result(int total, int score)
+	{
+		this.total = total;
+		this.score = score;
+	}
+	
+	public int getScore()
+	{
+		return this.score;
+	}
 
-    // Get
-    public String getPlayerName(){return playerName;}
-    public int getScore(){return score;}
-    public String getDifficultyLevel(){return difficultyLevel;}
-
-    // cal percentage
-    public double computePercentage() {
-        return (this.total == 0) ? 0 : (this.score * 100.0) / this.total;
-    }
-
+	// Public Methods
+	public double computePercentage()
+	{
+		return (this.score * 100.0) / this.total;
+	}
 }
